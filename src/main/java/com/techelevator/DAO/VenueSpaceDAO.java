cd4b96628c2e2339d9743a4d275d562df32e21a4
Joseph Hamrock <hamrocjr@miamioh.edu>
2021-10-21 14:16:58 -0400

    finished searchSpaceByDateAndOccupancy

--- src/main/java/com/techelevator/DAO/VenueSpaceDAO.java
+++ src/main/java/com/techelevator/DAO/VenueSpaceDAO.java
@@ -3,16 +3,17 @@ package com.techelevator.DAO;
 import com.techelevator.classes.Space;
 import com.techelevator.classes.Venue;
 
+import java.util.Date;
 import java.util.List;
 
 public interface VenueSpaceDAO {
 
     public List<Venue> viewVenues();
-    public Venue retrieveVenueDetails();
+    public Venue retrieveVenueDetails(long venueId);
     public List<Space> viewSpaces();
 
 
-    public boolean spaceIsAvailable();
+    public List<Space> searchSpaceByDateAndOccupancy(Date startDate, Date endDate, int occupancy );
 
     public Space updateAvailability();
 }
